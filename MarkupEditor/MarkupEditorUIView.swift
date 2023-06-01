f47815d50ce6f6ce633661dde7b583f58bc3a3bf
Faydee <faydee1220@gmail.com>
2023-06-01 11:08:54 +0800

    新增顏色參數

--- MarkupEditor/MarkupEditorUIView.swift
+++ MarkupEditor/MarkupEditorUIView.swift
@@ -19,7 +19,6 @@ import Combine
 /// In general, we don't want WebKit abstractions to leak into the MarkupEditor world. When the MarkupEditorUIView is instantiated,
 /// you can optionally specify the WKUIDelegate and WKNavigationDelegate if needed, which will be assigned to the underlying MarkupWKWebView.
 public class MarkupEditorUIView: UIView, MarkupDelegate {
-    var customColor: UIColor?
     private var toolbar: MarkupToolbarUIView!
     private var toolbarHeightConstraint: NSLayoutConstraint!
     private var webView: MarkupWKWebView!
@@ -37,6 +36,7 @@ public class MarkupEditorUIView: UIView, MarkupDelegate {
         userScripts: [String]? = nil,
         html: String?,
         placeholder: String? = nil,
+        tintColor: UIColor? = nil,
         selectAfterLoad: Bool = true,
         resourcesUrl: URL? = nil,
         id: String? = nil) {
@@ -44,8 +44,8 @@ public class MarkupEditorUIView: UIView, MarkupDelegate {
             webView = MarkupWKWebView(html: html, placeholder: placeholder, selectAfterLoad: selectAfterLoad, resourcesUrl: resourcesUrl, id: "Document", markupDelegate: markupDelegate ?? self)
             
             // 修改游標等的顏色
-            if let customColor = customColor {
-                webView.tintColor = customColor
+            if let tintColor = tintColor {
+                webView.tintColor = tintColor
             }
         
             // The coordinator acts as the WKScriptMessageHandler and will receive callbacks
